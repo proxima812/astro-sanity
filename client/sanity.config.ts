7c99b248dac60b52981c9226223b50fbd912e586
Kamil <116840531+proxima812@users.noreply.github.com>
2023-02-24 13:01:39 +0600

    Update sanity.config.ts

--- client/sanity.config.ts
+++ client/sanity.config.ts
@@ -6,7 +6,6 @@ import {schemaTypes} from './schemas'
 export default defineConfig({
   name: 'default',
   title: 'Админ Панель - АП',
-  basePath: 'https://ap-community.sanity.studio',
 
   projectId: 'dr4qv25t',
   dataset: 'production',
