bde90a030c7fbf34b6f85d6631b8469e6b7dac0a
Kamil <116840531+proxima812@users.noreply.github.com>
2023-02-24 13:21:25 +0600

    Update api.js

--- astro/src/libs/api.js
+++ astro/src/libs/api.js
@@ -51,7 +51,7 @@ export async function groupsGet() {
       group_time,
       body,
       group_url,
-    } | order(_createdAt desc)[0]
+    } | order(_createdAt asc)
  `;
  const groups = await useSanityClient().fetch(query);
  return groups;
